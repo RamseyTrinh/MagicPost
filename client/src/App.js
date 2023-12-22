47176830f28c9039d9ce617a6bcae261ec8e41fe
scott nguyen <scottnguyen282@gmail.com>
2023-12-22 13:47:53 +0700

    finish all role

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,11 @@ import PackageStatistics from "./Components/Panels/TransactionAdmin/PackageStati
 import GatherCreateAcc from "./Components/Panels/GatherAdmin/GatherCreateAcc.jsx";
 import GatherStatistics from "./Components/Panels/GatherAdmin/GatherStatistics.jsx";
 import GatherManage from "./Components/Panels/GatherAdmin/GatherManage.jsx";
+import AdminStatistics from "./Components/Panels/Admin/AdminStatistics.jsx";
+import ManagePoint from "./Components/Panels/Admin/ManagePoint/MangePoint.jsx";
+import ManagePointAcc from "./Components/Panels/Admin/ManagePointAcc/ManagePointAcc.jsx";
+import AccountInfo from "./Components/Panels/AcountInfo/AccountInfo.jsx";
+import Unauthorized from "./Components/Panels/Unauthorized/Unauthorized.jsx";
 import { useAuthUser } from "react-auth-kit";
 
 export default function App() {
@@ -31,21 +36,31 @@ export default function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/menu" element={<PrivateRoute />}>
-        <Route index element={role && <div>{role}</div>} />
+        <Route index element={role && <AccountInfo />} />
         <Route path="transaction">
           {(role === "transactionStaff" || role === "transactionAdmin") && (
             <>
               <Route
                 path="create"
-                element={role === "transactionStaff" ? <Form /> : null}
+                element={
+                  role === "transactionStaff" ? <Form /> : <Unauthorized />
+                }
               />
               <Route
                 path="transfer"
-                element={role === "transactionStaff" ? <Transfer /> : null}
+                element={
+                  role === "transactionStaff" ? <Transfer /> : <Unauthorized />
+                }
               />
               <Route
                 path="confirmation"
-                element={role === "transactionStaff" ? <Confirmation /> : null}
+                element={
+                  role === "transactionStaff" ? (
+                    <Confirmation />
+                  ) : (
+                    <Unauthorized />
+                  )
+                }
               />
               <Route
                 path="statistics"
@@ -54,13 +69,19 @@ export default function App() {
                     <Statistics />
                   ) : role === "transactionAdmin" ? (
                     <PackageStatistics />
-                  ) : null
+                  ) : (
+                    <Unauthorized />
+                  )
                 }
               />
               <Route
                 path="createAccount"
                 element={
-                  role === "transactionAdmin" ? <TransactionCreateAcc /> : null
+                  role === "transactionAdmin" ? (
+                    <TransactionCreateAcc />
+                  ) : (
+                    <Unauthorized />
+                  )
                 }
               />
             </>
@@ -71,26 +92,74 @@ export default function App() {
             <>
               <Route
                 path="transfer"
-                element={role === "warehouseStaff" ? <GatherTransfer /> : null}
+                element={
+                  role === "warehouseStaff" ? (
+                    <GatherTransfer />
+                  ) : (
+                    <Unauthorized />
+                  )
+                }
               />
               <Route
                 path="confirmation"
                 element={
-                  role === "warehouseStaff" ? <GatherConfirmation /> : null
+                  role === "warehouseStaff" ? (
+                    <GatherConfirmation />
+                  ) : (
+                    <Unauthorized />
+                  )
                 }
               />
               <Route
                 path="createAccount"
-                element={role === "warehouseAdmin" ? <GatherCreateAcc /> : null}
+                element={
+                  role === "warehouseAdmin" ? (
+                    <GatherCreateAcc />
+                  ) : (
+                    <Unauthorized />
+                  )
+                }
               />
               <Route
                 path="manage"
-                element={role === "warehouseAdmin" ? <GatherManage /> : null}
+                element={
+                  role === "warehouseAdmin" ? (
+                    <GatherManage />
+                  ) : (
+                    <Unauthorized />
+                  )
+                }
               />
               <Route
                 path="statistics"
                 element={
-                  role === "warehouseAdmin" ? <GatherStatistics /> : null
+                  role === "warehouseAdmin" ? (
+                    <GatherStatistics />
+                  ) : (
+                    <Unauthorized />
+                  )
+                }
+              />
+            </>
+          )}
+        </Route>
+        <Route path="admin">
+          {role === "admin" && (
+            <>
+              <Route
+                path="managePoint"
+                element={role === "admin" ? <ManagePoint /> : <Unauthorized />}
+              />
+              <Route
+                path="manageAccount"
+                element={
+                  role === "admin" ? <ManagePointAcc /> : <Unauthorized />
+                }
+              />
+              <Route
+                path="statistics"
+                element={
+                  role === "admin" ? <AdminStatistics /> : <Unauthorized />
                 }
               />
             </>
---
+++ client/src/Components/Panels/Admin/ManagePointAcc/CreatePointAdmin.tsx
@@ -0,0 +1,152 @@
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import React from "react";
+import SelectAdd from "../../../Funtions/SelectAdd/SelectAdd";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+// import MenuItem from "@mui/material/MenuItem";
+import { useForm } from "react-hook-form";
+import { Button } from "@mui/material";
+
+import "../../../../Assets/Styles/CreateAcc/CreateAcc.css";
+
+export default function CreatePointAdmin() {
+  type FormValues = {
+    pointAdminName: string;
+    phoneNumber: string;
+    address: string;
+    specificAdd: string;
+    dob: string;
+    email: string;
+    pointAssigned: string;
+    staffID: string;
+  };
+
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "80vh",
+        width: "100%",
+      }}
+    >
+      <Paper
+        sx={{
+          height: "700px",
+          width: "800px",
+          padding: 2,
+          backgroundColor: "#faf6ed",
+        }}
+        id="CreateAccMain"
+      >
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "30px",
+            color: "#003e29",
+            mb: 1,
+          }}
+        >
+          TẠO TÀI KHOẢN TRƯỞNG ĐIỂM
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack direction="column">
+            <Paper sx={{ height: "520px", padding: 3, background: "#fdfdfd" }}>
+              <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
+                <TextField
+                  id="outlined-basic"
+                  label="Họ và tên"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  {...register("pointAdminName")}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Số điện thoại"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  {...register("phoneNumber")}
+                />
+              </Stack>
+              <SelectAdd refs={{ ...register("address") }} />
+              <TextField
+                id="outlined-basic"
+                label="Địa chỉ"
+                variant="outlined"
+                fullWidth
+                required
+                sx={{ mb: 3 }}
+                {...register("specificAdd")}
+              />
+              <Stack direction="row" spacing={2} sx={{ mb: 3 }}>
+                <TextField
+                  label="Ngày sinh"
+                  InputLabelProps={{ shrink: true }}
+                  type="date"
+                  {...register("dob")}
+                  sx={{ width: "250px" }}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  sx={{ mb: 3 }}
+                  {...register("email")}
+                />
+              </Stack>
+              <Stack direction="row" spacing={2}>
+                <TextField
+                  id="outlined-basic"
+                  label="Mã nhân viên"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  {...register("staffID")}
+                />
+                <FormControl sx={{ width: "250px" }}>
+                  <InputLabel>Tại điểm</InputLabel>
+                  <Select
+                    defaultValue=""
+                    label="Tại điểm"
+                    //   {...register("pointType")}
+                  >
+                    {/* <MenuItem value={"GD"}>Điểm giao dịch</MenuItem>
+                    <MenuItem value={"TK"}>Điểm tập kết</MenuItem> */}
+                  </Select>
+                </FormControl>
+              </Stack>
+            </Paper>
+            <Button
+              variant="contained"
+              type="submit"
+              style={{
+                fontWeight: "bold",
+                background: "#003e29",
+                width: "150px",
+              }}
+              sx={{ mt: 4, ml: "auto", mr: "auto" }}
+            >
+              TẠO
+            </Button>
+          </Stack>
+        </form>
+      </Paper>
+    </div>
+  );
+}
