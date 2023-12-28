b8ae875735693776427b552605d4f8187ca40ea4
scott nguyen <scottnguyen282@gmail.com>
2023-12-28 11:15:04 +0700

    delete json

--- client/src/Components/Funtions/GeneratePDF/GeneratePDF.jsx
+++ client/src/Components/Funtions/GeneratePDF/GeneratePDF.jsx
@@ -9,34 +9,28 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   section: { textAlign: "center", margin: 20 },
+  label: { fontWeight: "bold", fontSize: 15 },
+  content: { fontSize: 10, marginLeft: 3, marginTop: 5 },
 });
 
 const MyDocument = () => (
   <Document>
     <Page size="A5" orientation="landscape" style={styles.page}>
       <View style={{ marginTop: 30 }}>
-        <View style={{ width: 540, height: 320, borderWidth: 1 }}>
+        <View style={{}}>
           <View style={{ display: "flex", flexDirection: "row" }}>
             <View style={{ display: "flex", flexDirection: "column" }}>
               <View style={{ width: 200, height: 70, borderWidth: 1 }}>
-                <Text style={{ fontWeight: "bold", fontSize: 15 }}>
-                  1. Người gửi
-                </Text>
-                <Text style={{}}>Họ và tên: {}</Text>
-                <Text style={{ marginTop: 3, fontSize: 10 }}>Địa chỉ: {}</Text>
-                <Text style={{ marginTop: 7, fontSize: 10 }}>
-                  Số điện thoại: {}
-                </Text>
+                <Text style={styles.label}>1. Người gửi</Text>
+                <Text style={styles.content}>Họ và tên: {}</Text>
+                <Text style={styles.content}>Địa chỉ: {}</Text>
+                <Text style={styles.content}>Số điện thoại: {}</Text>
               </View>
               <View style={{ width: 200, height: 70, borderWidth: 1 }}>
-                <Text style={{ fontWeight: "bold", fontSize: 15 }}>
-                  2. Người nhận
-                </Text>
-                <Text style={{}}>Họ và tên: {}</Text>
-                <Text style={{ marginTop: 3, fontSize: 10 }}>Địa chỉ: {}</Text>
-                <Text style={{ marginTop: 7, fontSize: 10 }}>
-                  Số điện thoại: {}
-                </Text>
+                <Text style={styles.label}>2. Người nhận</Text>
+                <Text style={styles.content}>Họ và tên: {}</Text>
+                <Text style={styles.content}>Địa chỉ: {}</Text>
+                <Text style={styles.content}>Số điện thoại: {}</Text>
               </View>
             </View>
             <View style={{ width: 340, height: 140, borderWidth: 1 }}></View>
